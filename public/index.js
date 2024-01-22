d8a442871393d116568d4311ddac9cef192e4771
Nathaniel Hughes <nathanielh2511@gmail.com>
2024-01-22 15:42:13 +0000

    Add server disconnect handling, rename variables to use camel case

--- public/index.js
+++ public/index.js
@@ -1,20 +1,25 @@
-const project_form = document.getElementById("project_form");
+const projectForm = document.getElementById("project_form");
 
 // https://github.com/stevenaeola/progblack_2324/blob/main/gordonramsay/client/fact-submit.js
-project_form.addEventListener("submit", async (event) => {
+projectForm.addEventListener("submit", async (event) => {
     event.preventDefault();
-    const formData = new FormData(project_form);
+    const formData = new FormData(projectForm);
 
     // Got URLSearchParams from ChatGPT
     const d = new URLSearchParams(formData).toString();
 
-    await fetch("http://127.0.0.1:8080/projects/create", {
+    const response = await fetch("http://127.0.0.1:8080/projects/create", {
         method: "POST",
         headers: {
             "Content-Type": "application/x-www-form-urlencoded"
         },
         body: d
     });
+
+    if (!response.ok) {
+        RenderError(response.status);
+        return;
+    }
 
     RenderProjects();
 })
@@ -28,72 +33,84 @@ function RenderError(code){
     document.getElementById('content').innerHTML=`<p style="width:50%; text-align:center; margin:auto">There was a problem fetching your content!</p>`;
 }
 
-function RenderHome(){
+async function RenderHome(){
     ChangeFavicon("assets/images/favicon.ico");
-    fetch('http://127.0.0.1:8080/intro')
-    .then(response => response.text())
-    .then(body => {
-        document.getElementById('title_text').innerText = "Nathaniel's Portfolio";
-        document.getElementById('content').innerHTML=`<p style="width:50%; text-align:center; margin:auto">${body}</p>`;
-    })
+    let response = {};
+    try {
+        response = await fetch(`http://127.0.0.1:8080/intro`);
+    } catch (error) {
+        RenderError("Could not connect to Server.");
+        return
+    }
+
+    if (!response.ok) {
+        RenderError(response.status);
+        return;
+    }
+    document.getElementById('title_text').innerText = "Nathaniel's Portfolio";
+    document.getElementById('content').innerHTML=`<p style="width:50%; text-align:center; margin:auto">${await response.text()}</p>`;
 }
 
-function RenderProject(id){
-    fetch(`http://127.0.0.1:8080/projects/project?id=${id}`)
-    .then(response => {
-        if (!response.ok) {
-            RenderError(response.status);
-            return;
-        }
-        return response.text();
+async function RenderProject(id){
+    let response = {};
+    try {
+        response = await fetch(`http://127.0.0.1:8080/projects/project?id=${id}`);
+    } catch (error) {
+        RenderError("Could not connect to Server.");
+        return
+    }
+          
+    if (!response.ok) {
+        RenderError(response.status);
+        return;
+    }
+
+    const project = await response.json();
+    ChangeFavicon(project.images[0]);
+    document.getElementById('title_text').innerText = "Projects:";
+    const content = document.createElement("div");
+    const images = document.createElement("div");
+    const row = document.createElement("div");
+    content.setAttribute("class", "col-lg-6");
+    images.setAttribute("class", "col-lg-6");
+    row.setAttribute("class", "row");
+    let tags = "";
+    project.tags.forEach(tag => {
+        tags += `<span class="badge text-bg-secondary">${tag}</span>\n`;
     })
-    .then(body => {
-        const project = JSON.parse(body);
-        ChangeFavicon(project.images[0]);
-        document.getElementById('title_text').innerText = "Projects:";
-        const content = document.createElement("div");
-        const images = document.createElement("div");
-        const row = document.createElement("div");
-        content.setAttribute("class", "col-lg-6");
-        images.setAttribute("class", "col-lg-6");
-        row.setAttribute("class", "row");
-        let tags = "";
-        project.tags.forEach(tag => {
-            tags += `<span class="badge text-bg-secondary">${tag}</span>\n`;
-        })
-        content.innerHTML = 
-        `
-            <h3>${project.title}</h3>
-            <p>${project.description}</p>
-            <br>
-            ${tags}
-        `;
-        project.images.forEach(image => {
-            images.innerHTML += `<img src=${image} alt="project image" style="margin:auto; padding:5px; max-width: 100%; height: auto;"/>`;
-        });
-        row.appendChild(content);
-        row.appendChild(images);
-        document.getElementById('content').replaceChildren(row);
-    })
+    content.innerHTML = 
+    `
+        <h3>${project.title}</h3>
+        <p>${project.description}</p>
+        <br>
+        ${tags}
+    `;
+    project.images.forEach(image => {
+        images.innerHTML += `<img src=${image} alt="project image" style="margin:auto; padding:5px; max-width: 100%; height: auto;"/>`;
+    });
+    row.appendChild(content);
+    row.appendChild(images);
+    document.getElementById('content').replaceChildren(row);
 }
 
-function RenderProjects(search = -1){
+async function RenderProjects(search = -1){
     ChangeFavicon("assets/images/favicon.ico");
-    fetch(`http://127.0.0.1:8080/projects/list?search=${encodeURIComponent(search)}`)
-    .then(response => {
-        if (!response.ok) {
-            RenderError(response.status);
-            return;
-        }
-        return response.text();
-    })
-    .then(body => {
-        document.getElementById('title_text').innerText = "Projects:";
-    const projects = JSON.parse(body)
-    let card_holder = document.createElement("div");
-    card_holder.setAttribute("id", "card_holder");
-    card_holder.setAttribute("class","row")
-    document.getElementById('content').innerText = body;
+    let response = {};
+    try {
+        response = await fetch(`http://127.0.0.1:8080/projects/list?search=${encodeURIComponent(search)}`);
+    } catch (error) {
+        RenderError("Could not connect to Server.");
+        return
+    }
+    if (!response.ok) {
+        RenderError(response.status);
+        return;
+    }
+    document.getElementById('title_text').innerText = "Projects:";
+    const projects = await response.json();
+    let cardHolder = document.createElement("div");
+    cardHolder.setAttribute("id", "card_holder");
+    cardHolder.setAttribute("class","row");
     projects.forEach(project => {
     let card = document.createElement("div");
     card.setAttribute("class", "card col-sm-4");
@@ -115,7 +132,7 @@ function RenderProjects(search = -1){
                 ${tags}
             </div>
         </div>`
-    card_holder.appendChild(card);
+    cardHolder.appendChild(card);
     });
 
     let card = document.createElement("div");
@@ -129,109 +146,144 @@ function RenderProjects(search = -1){
             <br>
             <a href="#projectModal" class="btn btn-primary" style="background-color: green" data-bs-toggle="modal">Create</a>
         </div>`
-    card_holder.appendChild(card);
+    cardHolder.appendChild(card);
 
-    document.getElementById('content').replaceChildren(card_holder);
-    })
+    document.getElementById('content').replaceChildren(cardHolder);
 }
 
-function RenderSkills(){
+async function RenderSkills(){
     ChangeFavicon("assets/images/favicon.ico");
-    fetch('http://127.0.0.1:8080/cv/skills')
-    .then(response => response.text())
-    .then(body => {
-        document.getElementById('title_text').innerText = "CV: Skills";
-        const skills = JSON.parse(body)
-        let skills_list = document.createElement("ul");
-        skills.forEach(skill => {
-            skill_element = document.createElement("li");
-            skill_element.innerText = skill;
-            skills_list.appendChild(skill_element);
-        });
-    document.getElementById('content').replaceChildren(skills_list);
-    })
-}
+    let response = {};
+    try {
+        response = await fetch(`http://127.0.0.1:8080/cv/skills`);
+    } catch (error) {
+        RenderError("Could not connect to Server.");
+        return
+    }
 
-function RenderEducation(){
-    ChangeFavicon("assets/images/favicon.ico");
-    fetch('http://127.0.0.1:8080/cv/education')
-    .then(response => response.text())
-    .then(body => {
-        document.getElementById('title_text').innerText = "CV: Education";
-        const education_data = JSON.parse(body)
-        let content = document.createElement("div");
-        education_data.forEach(data => {
-            let title = document.createElement("h4");
-            title.textContent = `${data.level} (${data.board}): ${data.school}`;
-            let results = document.createElement("ul");
-            for (var subject in data.results) {
-                result_data = document.createElement("li");
-                let subject_name = subject.replaceAll("_", " ");
-                subject_name = subject_name[0].toUpperCase() + subject_name.substring(1);
-                result_data.innerText = `${subject_name}: ${data.results[subject]}`;
-                results.appendChild(result_data);
-            }
-            content.appendChild(title);
-            content.appendChild(results);
-        });
-        document.getElementById('content').replaceChildren(content);
+    if (!response.ok) {
+        RenderError(response.status);
+        return;
+    }
+    document.getElementById('title_text').innerText = "CV: Skills";
+    const skills = await response.json();
+    let skillsList = document.createElement("ul");
+    skills.forEach(skill => {
+        const skillElement = document.createElement("li");
+        skillElement.innerText = skill;
+        skillsList.appendChild(skillElement);
     });
+    document.getElementById('content').replaceChildren(skillsList);
 }
 
-function RenderWorkExperience(){
+async function RenderEducation(){
     ChangeFavicon("assets/images/favicon.ico");
-    fetch('http://127.0.0.1:8080/cv/work_experience')
-    .then(response => response.text())
-    .then(body => {
-        document.getElementById('title_text').innerText = "CV: Work Experience";
-        const work_experience_data = JSON.parse(body);
-        let content = document.createElement("div");
-        work_experience_data.forEach(data => {
-            let company_name = document.createElement("h4");
-            company_name.innerText = data.company;
-            let details = document.createElement("p");
-            details.innerText = `Title: ${data.title} \n Duration: ${data.duration}`;
-            content.appendChild(company_name);
-            content.appendChild(details);
-        });
-        document.getElementById('content').replaceChildren(content);
-   });
-}
-
-function RenderHobbies(){
-    ChangeFavicon("assets/images/favicon.ico");
-    fetch('http://127.0.0.1:8080/cv/hobbies')
-    .then(response => response.text())
-    .then(body => {
-        document.getElementById('title_text').innerText = "CV: Hobbies";
-        const hobbies = JSON.parse(body)
-        let hobbies_list = document.createElement("ul");
-        hobbies.forEach(hobby => {
-            hobby_element = document.createElement("li");
-            hobby_element.innerText = hobby;
-            hobbies_list.appendChild(hobby_element);
-        });
-       document.getElementById('content').replaceChildren(hobbies_list);
-    })
-}
-
-function RenderContact(){
-    ChangeFavicon("assets/images/favicon.ico");
-    fetch('http://127.0.0.1:8080/cv/contact')
-    .then(response => response.text())
-    .then(body => {
-        document.getElementById('title_text').innerText = "CV: Contact";
-        const info = JSON.parse(body);
-        let info_list = document.createElement("ul");
-        for(var mode in info){
-            let name = mode.replaceAll("_", " ");
-            name = name[0].toUpperCase() + name.substring(1);
-            let info_element = document.createElement("li");
-            info_element.innerText = `${name}: ${info[mode]}`
-            info_list.appendChild(info_element);
+    let response = {};
+    try {
+        response = await fetch(`http://127.0.0.1:8080/cv/education`);
+    } catch (error) {
+        RenderError("Could not connect to Server.");
+        return
+    }
+    if (!response.ok) {
+        RenderError(response.status);
+        return;
+    }
+    document.getElementById('title_text').innerText = "CV: Education";
+    const educationData = await response.json();
+    let content = document.createElement("div");
+    educationData.forEach(data => {
+        let title = document.createElement("h4");
+        title.textContent = `${data.level} (${data.board}): ${data.school}`;
+        let results = document.createElement("ul");
+        for (var subject in data.results) {
+            let resultData = document.createElement("li");
+            let subjectName = subject.replaceAll("_", " ");
+            subjectName = subjectName[0].toUpperCase() + subjectName.substring(1);
+            resultData.innerText = `${subjectName}: ${data.results[subject]}`;
+            results.appendChild(resultData);
         }
-       document.getElementById('content').replaceChildren(info_list);
-    })
+        content.appendChild(title);
+        content.appendChild(results);
+    });
+    document.getElementById('content').replaceChildren(content);
+}
+
+async function RenderWorkExperience(){
+    ChangeFavicon("assets/images/favicon.ico");
+    let response = {};
+    try {
+        response = await fetch(`http://127.0.0.1:8080/cv/work_experience`);
+    } catch (error) {
+        RenderError("Could not connect to Server.");
+        return
+    }
+    if (!response.ok) {
+        RenderError(response.status);
+        return;
+    }
+    document.getElementById('title_text').innerText = "CV: Work Experience";
+    const workExperienceData = await response.json();
+    let content = document.createElement("div");
+    workExperienceData.forEach(data => {
+        let companyName = document.createElement("h4");
+        companyName.innerText = data.company;
+        let details = document.createElement("p");
+        details.innerText = `Title: ${data.title} \n Duration: ${data.duration}`;
+        content.appendChild(companyName);
+        content.appendChild(details);
+    });
+    document.getElementById('content').replaceChildren(content);
+}
+
+async function RenderHobbies(){
+    ChangeFavicon("assets/images/favicon.ico");
+    let response = {};
+    try {
+        response = await fetch(`http://127.0.0.1:8080/cv/hobbies`);
+    } catch (error) {
+        RenderError("Could not connect to Server.");
+        return
+    }
+    if (!response.ok) {
+        RenderError(response.status);
+        return;
+    }
+    document.getElementById('title_text').innerText = "CV: Hobbies";
+    const hobbies = await response.json();
+    let hobbiesList = document.createElement("ul");
+    hobbies.forEach(hobby => {
+        let hobbyElement = document.createElement("li");
+        hobbyElement.innerText = hobby;
+        hobbiesList.appendChild(hobbyElement);
+    });
+    document.getElementById('content').replaceChildren(hobbiesList);
+}
+
+async function RenderContact(){
+    ChangeFavicon("assets/images/favicon.ico");
+    let response = {};
+    try {
+        response = await fetch(`http://127.0.0.1:8080/cv/contact`);
+    } catch (error) {
+        RenderError("Could not connect to Server.");
+        return
+    }
+    if (!response.ok) {
+        RenderError(response.status);
+        return;
+    }
+    document.getElementById('title_text').innerText = "CV: Contact";
+    const info = await response.json();
+    let infoList = document.createElement("ul");
+    for(var mode in info){
+        let name = mode.replaceAll("_", " ");
+        name = name[0].toUpperCase() + name.substring(1);
+        let infoElement = document.createElement("li");
+        infoElement.innerText = `${name}: ${info[mode]}`
+        infoList.appendChild(infoElement);
+    }
+    document.getElementById('content').replaceChildren(infoList);
 }
 
 // https://stackoverflow.com/questions/260857/changing-website-favicon-dynamically
